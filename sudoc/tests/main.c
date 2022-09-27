8d59cdff1c73072cdee00a161e240b8dc61cf4a5
mickael-btc <mickael.bobovitch@epita.fr>
2022-09-27 21:03:21 +0200

    refactored and added better way to add function for testing

--- sudoc/tests/main.c
+++ sudoc/tests/main.c
@@ -1,19 +1,30 @@
+#include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "include/test_add.h"
 
-// by default the test are successful
-int ret = 0;
+//lists of all tests to run
+int (*tests[])() = {
+    test_add_positive,
+    test_add_negative,
+};
 
 int main()
-{
-    // add here all the tests
-    ret = test_add_positive() || ret;
-    ret = test_add_negative() || ret;
-    
-    // if at least one test fails
-    if (ret)
-        return EXIT_FAILURE;
-    // if every tests are successful
-    else 
-        return EXIT_SUCCESS;
+{   
+    // start timer
+    clock_t begin = clock();
+
+    //run all tests
+    int test_count = sizeof(tests) / sizeof(tests[0]);
+    int failed = 0;
+
+    for (int i = 0; i < test_count; i++)
+        failed += tests[i]();
+
+    // stop timer
+    clock_t end = clock();
+    double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+
+    printf("Ran %d tests in %f seconds. %d failed.\n", test_count, time_spent, failed);
+    return EXIT_SUCCESS;
 }
