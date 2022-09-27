3d8ad64dfec715c26ad149bfac6d759c9f08f8d1
mickael-btc <mickael.bobovitch@epita.fr>
2022-09-27 21:01:51 +0200

    added better formating for tests

--- sudoc/libs/src/utils.c
+++ sudoc/libs/src/utils.c
@@ -1,18 +1,28 @@
 #include <stdio.h>
 #include "../include/utils.h"
 
+// Color codes for printing
+#define RESET   "\033[0m"
+#define BLACK   "\033[30m"
+#define RED     "\033[31m"
+#define GREEN   "\033[32m"
+#define YELLOW  "\033[33m"
+#define BLUE    "\033[34m"
+#define MAGENTA "\033[35m"
+#define CYAN    "\033[36m"
+#define WHITE   "\033[37m"
+
 int print_test(int result, int expected, char name[])
 {
-
     if (result == expected)
     {
-        printf("%s: Success\n", name);
+        printf(GREEN "%s: Success\n" RESET, name);
         return 0;
     }
 
     else
     {
-        printf("%s: Test failed, got %i, expected %i\n", name, result, expected);
+        printf(RED "%s: Test failed, got %i, expected %i\n" RESET, name, result, expected);
         return 1;
     }
 }
