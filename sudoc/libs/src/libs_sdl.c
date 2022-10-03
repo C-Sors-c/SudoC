91f4b3964ce83f3d9c67743d500610609013ed30
Noé Susset <noe.susset@epita.fr>
2022-10-03 20:33:18 +0200

    added the lock surface

--- sudoc/libs/src/libs_sdl.c
+++ sudoc/libs/src/libs_sdl.c
@@ -6,8 +6,11 @@ with rows column and rgb for each dimension. RGB in 0-255 format.
 */
 unsigned char ***sdl_surface_to_array(SDL_Surface *surface)
 {
+    SDL_LockSurface(surface);
     int w = surface->w;
     int h = surface->h;
+    //save the format of the surface
+    SDL_PixelFormat *format = surface->format;
     Uint32* pixels = surface->pixels;
     unsigned char ***array = malloc(h * sizeof(unsigned char **));
     for (int i = 0; i < h; i++)
@@ -17,8 +20,9 @@ unsigned char ***sdl_surface_to_array(SDL_Surface *surface)
         {
             array[i][j] = malloc(3 * sizeof(unsigned char));
             Uint32 pixel = pixels[i * w + j];
-            SDL_GetRGB(pixel, surface->format, &array[i][j][0], &array[i][j][1], &array[i][j][2]);
+            SDL_GetRGB(pixel, format, &array[i][j][0], &array[i][j][1], &array[i][j][2]);
         }
     }
+    SDL_UnlockSurface(surface);
     return array;
 }
