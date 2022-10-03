0be1203e00b70e8f5dfd3f4aaef148623e1ea1e2
Maxime Ellerbach <maxime@ellerbach.net>
2022-10-04 00:43:46 +0200

    Matrix4 + some NN parts (#21)
    * first draft, work in progress
    
    * saving stuff before getting into messy stuff
    
    * finished matrix4 ops + some structure + some tests

--- tests/include/test_matrix.h
+++ tests/include/test_matrix.h
@@ -7,3 +7,10 @@ int test_matrix_add();
 int test_matrix_subtract();
 int test_matrix_multiply();
 int test_matrix_multiply_scalar();
+int test_matrix_transpose();
+
+int test_matrix4_add();
+int test_matrix4_subtract();
+int test_matrix4_multiply();
+int test_matrix4_multiply_scalar();
+int test_matrix4_transpose();
