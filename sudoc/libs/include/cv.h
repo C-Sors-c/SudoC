8ff7c0a8fec676f3390f8eb5a2f9e0b495b5ab7c
mickael-btc <mickael.bobovitch@epita.fr>
2022-10-05 00:08:22 +0200

    added functions

--- sudoc/libs/include/cv.h
+++ sudoc/libs/include/cv.h
@@ -1,40 +1,56 @@
 #pragma once
 
-typedef struct {
+typedef struct
+{
     int width;
     int height;
-    char*** data;
+    unsigned char ***data;
 } cv_image_rgb;
 
-typedef struct {
+typedef struct
+{
     int width;
     int height;
-    float*** data;
+    float ***data;
 } cv_image_rgb_normalized;
 
-typedef struct {
+typedef struct
+{
     int width;
     int height;
-    char** data;
+    unsigned char **data;
 } cv_image_gray;
 
-typedef struct {
+typedef struct
+{
     int width;
     int height;
-    float** data;
+    float **data;
 } cv_image_gray_normalized;
 
-void cv_init_image_rgb(cv_image_rgb* image, int width, int height);
-void cv_free_image_rgb(cv_image_rgb* image);
-void cv_init_image_gray(cv_image_gray* image, int width, int height);
-void cv_free_image_gray(cv_image_gray* image);
-void cv_init_image_rgb_normalized(cv_image_rgb_normalized* image, int width, int height);
-void cv_free_image_rgb_normalized(cv_image_rgb_normalized* image);
-void cv_init_image_gray_normalized(cv_image_gray_normalized* image, int width, int height);
-void cv_free_image_gray_normalized(cv_image_gray_normalized* image);
-void cv_rgb_to_gray(cv_image_rgb* rgb, cv_image_gray* gray);
-void cv_gray_to_rgb(cv_image_gray* gray, cv_image_rgb* rgb);
-void cv_normalize_image_rgb(cv_image_rgb* rgb, cv_image_rgb_normalized* rgb_normalized);
-void cv_normalize_image_gray(cv_image_gray* gray, cv_image_gray_normalized* gray_normalized);
-void cv_denormalize_image_rgb(cv_image_rgb_normalized* rgb_normalized, cv_image_rgb* rgb);
-void cv_denormalize_image_gray(cv_image_gray_normalized* gray_normalized, cv_image_gray* gray);
+cv_image_rgb *cv_init_image_rgb(int width, int height);
+cv_image_rgb *cv_copy_image_rgb(unsigned char ***image, int width, int height);
+void cv_free_image_rgb(cv_image_rgb *image);
+
+cv_image_gray *cv_init_image_gray(int width, int height);
+cv_image_gray *cv_copy_image_gray(unsigned char **image, int width, int height);
+void cv_free_image_gray(cv_image_gray *image);
+
+cv_image_rgb_normalized *cv_init_image_rgb_normalized(int width, int height);
+cv_image_rgb_normalized *cv_copy_image_rgb_normalized(float ***image, int width, int height);
+void cv_free_image_rgb_normalized(cv_image_rgb_normalized *image);
+
+cv_image_gray_normalized *cv_init_image_gray_normalized(int width, int height);
+cv_image_gray_normalized *cv_copy_image_gray_normalized(float **image, int width, int height);
+void cv_free_image_gray_normalized(cv_image_gray_normalized *image);
+
+void cv_rgb_to_gray(cv_image_rgb *rgb, cv_image_gray *gray);
+void cv_gray_to_rgb(cv_image_gray *gray, cv_image_rgb *rgb);
+
+void cv_normalize_image_rgb(cv_image_rgb *rgb, cv_image_rgb_normalized *rgb_normalized);
+void cv_normalize_image_gray(cv_image_gray *gray, cv_image_gray_normalized *gray_normalized);
+void cv_denormalize_image_rgb(cv_image_rgb_normalized *rgb_normalized, cv_image_rgb *rgb);
+void cv_denormalize_image_gray(cv_image_gray_normalized *gray_normalized, cv_image_gray *gray);
+
+void cv_grayscale(cv_image_rgb *rgb);
+void cv_gaussian_blur(cv_image_gray *src, cv_image_gray *dst, int kernel_size, double sigma);
--- sudoc/libs/src/cv.c
+++ sudoc/libs/src/cv.c
@@ -4,33 +4,61 @@
 #include <math.h>
 
 /// @brief Allocate memory for a cv_image_rgb structure.
-/// @param image Pointer to the cv_image_rgb structure.
 /// @param width Width of the image.
 /// @param height Height of the image.
 /// @return void
-void cv_init_image_rgb(cv_image_rgb* image, int width, int height) {
+cv_image_rgb *cv_init_image_rgb(int width, int height)
+{
+    cv_image_rgb *image = malloc(sizeof(cv_image_rgb));
+
     image->width = width;
     image->height = height;
-    image->data = malloc(sizeof(char**) * height);
+    image->data = malloc(sizeof(unsigned char **) * height);
 
-    if (image->data == NULL) {
-        errx(EXIT_FAILURE, "Error: malloc failed in cv_init_image_rgb (1) (width=%d, height=%d)", width, height);
+    for (int i = 0; i < height; i++)
+    {
+        image->data[i] = malloc(sizeof(unsigned char *) * width);
+        for (int j = 0; j < width; j++)
+        {
+            image->data[i][j] = malloc(sizeof(unsigned char) * 3);
+        }
     }
 
-    for (int i = 0; i < height; i++) {
-        image->data[i] = malloc(sizeof(char*) * width);
-        for (int j = 0; j < width; j++) {
-            image->data[i][j] = malloc(sizeof(char) * 3);
+    return image;
+}
+
+/// @brief Copy an image from an array to a cv_image_rgb structure.
+/// @param image Array containing the image.
+/// @param width Width of the image.
+/// @param height Height of the image.
+/// @return Returns a pointer to the cv_image_rgb structure.
+cv_image_rgb *cv_copy_image_rgb(unsigned char ***image, int width, int height)
+{
+    cv_image_rgb *copy = cv_init_image_rgb(width, height);
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            for (int k = 0; k < 3; k++)
+            {
+                copy->data[i][j][k] = image[i][j][k];
+            }
         }
-    }    
+    }
+
+    return copy;
 }
 
 /// @brief Free memory allocated for a cv_image_rgb structure.
 /// @param image Pointer to the cv_image_rgb structure.
 /// @return void
-void cv_free_image_rgb(cv_image_rgb* image) {
-    for (int i = 0; i < image->height; i++) {
-        for (int j = 0; j < image->width; j++) {
+void cv_free_image_rgb(cv_image_rgb *image)
+{
+    for (int i = 0; i < image->height; i++)
+    {
+        for (int j = 0; j < image->width; j++)
+        {
             free(image->data[i][j]);
         }
         free(image->data[i]);
@@ -43,25 +71,49 @@ void cv_free_image_rgb(cv_image_rgb* image) {
 /// @param width Width of the image.
 /// @param height Height of the image.
 /// @return void
-void cv_init_image_gray(cv_image_gray* image, int width, int height) {
+cv_image_gray *cv_init_image_gray(int width, int height)
+{
+    cv_image_gray *image = malloc(sizeof(cv_image_gray));
+
     image->width = width;
     image->height = height;
-    image->data = malloc(sizeof(char*) * height);
+    image->data = malloc(sizeof(unsigned char *) * height);
 
-    if (image->data == NULL) {
-        errx(EXIT_FAILURE, "Error: malloc failed in cv_init_image_gray (1) (width=%d, height=%d)", width, height);
+    for (int i = 0; i < height; i++)
+    {
+        image->data[i] = malloc(sizeof(unsigned char) * width);
     }
 
-    for (int i = 0; i < height; i++) {
-        image->data[i] = malloc(sizeof(char) * width);
-    }    
+    return image;
+}
+
+/// @brief Copy an image from an array to a cv_image_gray structure.
+/// @param image Array containing the image.
+/// @param width Width of the image.
+/// @param height Height of the image.
+/// @return Returns a pointer to the cv_image_gray structure.
+cv_image_gray *cv_copy_image_gray(unsigned char **image, int width, int height)
+{
+    cv_image_gray *copy = cv_init_image_gray(width, height);
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            copy->data[i][j] = image[i][j];
+        }
+    }
+
+    return copy;
 }
 
 /// @brief Free memory allocated for a cv_image_rgb_gray structure.
 /// @param image Pointer to the cv_image_rgb_gray structure.
 /// @return void
-void cv_free_image_gray(cv_image_gray* image) {
-    for (int i = 0; i < image->height; i++) {
+void cv_free_image_gray(cv_image_gray *image)
+{
+    for (int i = 0; i < image->height; i++)
+    {
         free(image->data[i]);
     }
     free(image->data);
@@ -72,29 +124,58 @@ void cv_free_image_gray(cv_image_gray* image) {
 /// @param width Width of the image.
 /// @param height Height of the image.
 /// @return void
-void cv_init_image_rgb_normalized(cv_image_rgb_normalized* image, int width, int height) {
+cv_image_rgb_normalized *cv_init_image_rgb_normalized(int width, int height)
+{
+    cv_image_rgb_normalized *image = malloc(sizeof(cv_image_rgb_normalized));
+
     image->width = width;
     image->height = height;
-    image->data = malloc(sizeof(float**) * height);
+    image->data = malloc(sizeof(float **) * height);
 
-    if (image->data == NULL) {
-        errx(EXIT_FAILURE, "Error: malloc failed in cv_init_image_rgb_normalized (1) (width=%d, height=%d)", width, height);
-    }
-
-    for (int i = 0; i < height; i++) {
-        image->data[i] = malloc(sizeof(float*) * width);
-        for (int j = 0; j < width; j++) {
+    for (int i = 0; i < height; i++)
+    {
+        image->data[i] = malloc(sizeof(float *) * width);
+        for (int j = 0; j < width; j++)
+        {
             image->data[i][j] = malloc(sizeof(float) * 3);
         }
-    }    
+    }
+
+    return image;
+}
+
+/// @brief Copy an image from an array to a cv_image_rgb_normalized structure.
+/// @param image Array containing the image.
+/// @param width Width of the image.
+/// @param height Height of the image.
+/// @return Returns a pointer to the cv_image_rgb_normalized structure.
+cv_image_rgb_normalized *cv_copy_image_rgb_normalized(float ***image, int width, int height)
+{
+    cv_image_rgb_normalized *copy = cv_init_image_rgb_normalized(width, height);
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            for (int k = 0; k < 3; k++)
+            {
+                copy->data[i][j][k] = image[i][j][k];
+            }
+        }
+    }
+
+    return copy;
 }
 
 /// @brief Free memory allocated for a cv_image_rgb_normalized structure.
 /// @param image Pointer to the cv_image_rgb_normalized structure.
 /// @return void
-void cv_free_image_rgb_normalized(cv_image_rgb_normalized* image) {
-    for (int i = 0; i < image->height; i++) {
-        for (int j = 0; j < image->width; j++) {
+void cv_free_image_rgb_normalized(cv_image_rgb_normalized *image)
+{
+    for (int i = 0; i < image->height; i++)
+    {
+        for (int j = 0; j < image->width; j++)
+        {
             free(image->data[i][j]);
         }
         free(image->data[i]);
@@ -107,25 +188,49 @@ void cv_free_image_rgb_normalized(cv_image_rgb_normalized* image) {
 /// @param width Width of the image.
 /// @param height Height of the image.
 /// @return void
-void cv_init_image_gray_normalized(cv_image_gray_normalized* image, int width, int height) {
+cv_image_gray_normalized *cv_init_image_gray_normalized(int width, int height)
+{
+    cv_image_gray_normalized *image = malloc(sizeof(cv_image_gray_normalized));
+
     image->width = width;
     image->height = height;
-    image->data = malloc(sizeof(float*) * height);
+    image->data = malloc(sizeof(float *) * height);
 
-    if (image->data == NULL) {
-        errx(EXIT_FAILURE, "Error: malloc failed in cv_init_image_gray_normalized (1) (width=%d, height=%d)", width, height);
+    for (int i = 0; i < height; i++)
+    {
+        image->data[i] = malloc(sizeof(float) * width);
     }
 
-    for (int i = 0; i < height; i++) {
-        image->data[i] = malloc(sizeof(float) * width);
-    }    
+    return image;
+}
+
+/// @brief Copy an image from an array to a cv_image_gray_normalized structure.
+/// @param image Array containing the image.
+/// @param width Width of the image.
+/// @param height Height of the image.
+/// @return Returns a pointer to the cv_image_gray_normalized structure.
+cv_image_gray_normalized *cv_copy_image_gray_normalized(float **image, int width, int height)
+{
+    cv_image_gray_normalized *copy = cv_init_image_gray_normalized(width, height);
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            copy->data[i][j] = image[i][j];
+        }
+    }
+
+    return copy;
 }
 
 /// @brief Free memory allocated for a cv_image_gray_normalized structure.
 /// @param image Pointer to the cv_image_gray_normalized structure.
 /// @return void
-void cv_free_image_gray_normalized(cv_image_gray_normalized* image) {
-    for (int i = 0; i < image->height; i++) {
+void cv_free_image_gray_normalized(cv_image_gray_normalized *image)
+{
+    for (int i = 0; i < image->height; i++)
+    {
         free(image->data[i]);
     }
     free(image->data);
@@ -135,13 +240,17 @@ void cv_free_image_gray_normalized(cv_image_gray_normalized* image) {
 /// @param rgb Pointer to the cv_image_rgb structure.
 /// @param gray Pointer to the cv_image_gray structure.
 /// @return void
-void cv_rgb_to_gray(cv_image_rgb* rgb, cv_image_gray* gray) {
-    if (rgb->width != gray->width || rgb->height != gray->height) {
+void cv_rgb_to_gray(cv_image_rgb *rgb, cv_image_gray *gray)
+{
+    if (rgb->width != gray->width || rgb->height != gray->height)
+    {
         errx(EXIT_FAILURE, "Error: cv_rgb_to_gray: image sizes do not match (rgb: %dx%d, gray: %dx%d)", rgb->width, rgb->height, gray->width, gray->height);
     }
 
-    for (int i = 0; i < rgb->height; i++) {
-        for (int j = 0; j < rgb->width; j++) {
+    for (int i = 0; i < rgb->height; i++)
+    {
+        for (int j = 0; j < rgb->width; j++)
+        {
             gray->data[i][j] = (rgb->data[i][j][0] + rgb->data[i][j][1] + rgb->data[i][j][2]) / 3;
         }
     }
@@ -151,13 +260,17 @@ void cv_rgb_to_gray(cv_image_rgb* rgb, cv_image_gray* gray) {
 /// @param gray Pointer to the cv_image_gray structure.
 /// @param rgb Pointer to the cv_image_rgb structure.
 /// @return void
-void cv_gray_to_rgb(cv_image_gray* gray, cv_image_rgb* rgb) {
-    if (rgb->width != gray->width || rgb->height != gray->height) {
+void cv_gray_to_rgb(cv_image_gray *gray, cv_image_rgb *rgb)
+{
+    if (rgb->width != gray->width || rgb->height != gray->height)
+    {
         errx(EXIT_FAILURE, "Error: cv_gray_to_rgb: image sizes do not match (rgb: %dx%d, gray: %dx%d)", rgb->width, rgb->height, gray->width, gray->height);
     }
 
-    for (int i = 0; i < rgb->height; i++) {
-        for (int j = 0; j < rgb->width; j++) {
+    for (int i = 0; i < rgb->height; i++)
+    {
+        for (int j = 0; j < rgb->width; j++)
+        {
             rgb->data[i][j][0] = gray->data[i][j];
             rgb->data[i][j][1] = gray->data[i][j];
             rgb->data[i][j][2] = gray->data[i][j];
@@ -169,13 +282,17 @@ void cv_gray_to_rgb(cv_image_gray* gray, cv_image_rgb* rgb) {
 /// @param rgb Pointer to the cv_image_rgb structure.
 /// @param rgb_normalized Pointer to the cv_image_rgb_normalized structure.
 /// @return void
-void cv_normalize_image_rgb(cv_image_rgb* rgb, cv_image_rgb_normalized* rgb_normalized) {
-    if (rgb->width != rgb_normalized->width || rgb->height != rgb_normalized->height) {
+void cv_normalize_image_rgb(cv_image_rgb *rgb, cv_image_rgb_normalized *rgb_normalized)
+{
+    if (rgb->width != rgb_normalized->width || rgb->height != rgb_normalized->height)
+    {
         errx(EXIT_FAILURE, "Error: cv_normalize_image_rgb: image sizes do not match (rgb: %dx%d, rgb_normalized: %dx%d)", rgb->width, rgb->height, rgb_normalized->width, rgb_normalized->height);
     }
 
-    for (int i = 0; i < rgb->height; i++) {
-        for (int j = 0; j < rgb->width; j++) {
+    for (int i = 0; i < rgb->height; i++)
+    {
+        for (int j = 0; j < rgb->width; j++)
+        {
             rgb_normalized->data[i][j][0] = rgb->data[i][j][0] / 255.0;
             rgb_normalized->data[i][j][1] = rgb->data[i][j][1] / 255.0;
             rgb_normalized->data[i][j][2] = rgb->data[i][j][2] / 255.0;
@@ -187,13 +304,17 @@ void cv_normalize_image_rgb(cv_image_rgb* rgb, cv_image_rgb_normalized* rgb_norm
 /// @param gray Pointer to the cv_image_gray structure.
 /// @param gray_normalized Pointer to the cv_image_gray_normalized structure.
 /// @return void
-void cv_normalize_image_gray(cv_image_gray* gray, cv_image_gray_normalized* gray_normalized) {
-    if (gray->width != gray_normalized->width || gray->height != gray_normalized->height) {
+void cv_normalize_image_gray(cv_image_gray *gray, cv_image_gray_normalized *gray_normalized)
+{
+    if (gray->width != gray_normalized->width || gray->height != gray_normalized->height)
+    {
         errx(EXIT_FAILURE, "Error: cv_normalize_image_gray: image sizes do not match (gray: %dx%d, gray_normalized: %dx%d)", gray->width, gray->height, gray_normalized->width, gray_normalized->height);
     }
 
-    for (int i = 0; i < gray->height; i++) {
-        for (int j = 0; j < gray->width; j++) {
+    for (int i = 0; i < gray->height; i++)
+    {
+        for (int j = 0; j < gray->width; j++)
+        {
             gray_normalized->data[i][j] = gray->data[i][j] / 255.0;
         }
     }
@@ -203,13 +324,17 @@ void cv_normalize_image_gray(cv_image_gray* gray, cv_image_gray_normalized* gray
 /// @param rgb_normalized Pointer to the cv_image_rgb_normalized structure.
 /// @param rgb Pointer to the cv_image_rgb structure.
 /// @return void
-void cv_denormalize_image_rgb(cv_image_rgb_normalized* rgb_normalized, cv_image_rgb* rgb) {
-    if (rgb->width != rgb_normalized->width || rgb->height != rgb_normalized->height) {
+void cv_denormalize_image_rgb(cv_image_rgb_normalized *rgb_normalized, cv_image_rgb *rgb)
+{
+    if (rgb->width != rgb_normalized->width || rgb->height != rgb_normalized->height)
+    {
         errx(EXIT_FAILURE, "Error: cv_denormalize_image_rgb: image sizes do not match (rgb: %dx%d, rgb_normalized: %dx%d)", rgb->width, rgb->height, rgb_normalized->width, rgb_normalized->height);
     }
 
-    for (int i = 0; i < rgb->height; i++) {
-        for (int j = 0; j < rgb->width; j++) {
+    for (int i = 0; i < rgb->height; i++)
+    {
+        for (int j = 0; j < rgb->width; j++)
+        {
             rgb->data[i][j][0] = rgb_normalized->data[i][j][0] * 255.0;
             rgb->data[i][j][1] = rgb_normalized->data[i][j][1] * 255.0;
             rgb->data[i][j][2] = rgb_normalized->data[i][j][2] * 255.0;
@@ -221,48 +346,76 @@ void cv_denormalize_image_rgb(cv_image_rgb_normalized* rgb_normalized, cv_image_
 /// @param gray_normalized Pointer to the cv_image_gray_normalized structure.
 /// @param gray Pointer to the cv_image_gray structure.
 /// @return void
-void cv_denormalize_image_gray(cv_image_gray_normalized* gray_normalized, cv_image_gray* gray) {
-    if (gray->width != gray_normalized->width || gray->height != gray_normalized->height) {
+void cv_denormalize_image_gray(cv_image_gray_normalized *gray_normalized, cv_image_gray *gray)
+{
+    if (gray->width != gray_normalized->width || gray->height != gray_normalized->height)
+    {
         errx(EXIT_FAILURE, "Error: cv_denormalize_image_gray: image sizes do not match (gray: %dx%d, gray_normalized: %dx%d)", gray->width, gray->height, gray_normalized->width, gray_normalized->height);
     }
 
-    for (int i = 0; i < gray->height; i++) {
-        for (int j = 0; j < gray->width; j++) {
+    for (int i = 0; i < gray->height; i++)
+    {
+        for (int j = 0; j < gray->width; j++)
+        {
             gray->data[i][j] = gray_normalized->data[i][j] * 255.0;
         }
     }
 }
 
+/// @brief Apply grayscale to a cv_image_rgb structure.
+/// @param rgb Pointer to the cv_image_rgb structure.
+void cv_grayscale(cv_image_rgb *rgb)
+{
+    cv_image_gray *gray = cv_init_image_gray(rgb->width, rgb->height);
+    cv_rgb_to_gray(rgb, gray);
+    cv_gray_to_rgb(gray, rgb);
+    cv_free_image_gray(gray);
+}
 
-void cv_gaussian_blur(cv_image_gray* src, cv_image_gray* dst, int kernel_size, double sigma) {
-    if (src->width != dst->width || src->height != dst->height) {
+/// @brief Apply a Gaussian blur to a cv_image_rgb structure.
+/// @param src Pointer to the cv_image_gray structure.
+/// @param dst Pointer to the cv_image_gray structure.
+/// @param kernel_size Size of the Gaussian kernel.
+/// @param sigma Standard deviation of the Gaussian kernel.
+void cv_gaussian_blur(cv_image_gray *src, cv_image_gray *dst, int kernel_size, double sigma)
+{
+    if (src->width != dst->width || src->height != dst->height)
+    {
         errx(EXIT_FAILURE, "Error: cv_gaussian_blur: image sizes do not match (src: %dx%d, dst: %dx%d)", src->width, src->height, dst->width, dst->height);
     }
-    
-    if (kernel_size % 2 == 0) {
+
+    if (kernel_size % 2 == 0)
+    {
         errx(EXIT_FAILURE, "Error: cv_gaussian_blur: kernel_size must be odd");
     }
 
-    for (int i = 0; i < src->height; i++) {
-        for (int j = 0; j < src->width; j++) {
+    if (sigma <= 0)
+    {
+        sigma = (kernel_size - 1) / 6.0;
+    }
+
+    for (int i = 0; i < src->height; i++)
+    {
+        for (int j = 0; j < src->width; j++)
+        {
             double sum = 0.0;
             double weight_sum = 0.0;
-            for (int k = -kernel_size / 2; k <= kernel_size / 2; k++) {
-                for (int l = -kernel_size / 2; l <= kernel_size / 2; l++) {
+            for (int k = -kernel_size / 2; k <= kernel_size / 2; k++)
+            {
+                for (int l = -kernel_size / 2; l <= kernel_size / 2; l++)
+                {
                     int x = j + l;
                     int y = i + k;
-                    if (x < 0) {
+
+                    if (x < 0)
                         x = 0;
-                    }
-                    if (x >= src->width) {
+                    if (x >= src->width)
                         x = src->width - 1;
-                    }
-                    if (y < 0) {
+                    if (y < 0)
                         y = 0;
-                    }
-                    if (y >= src->height) {
+                    if (y >= src->height)
                         y = src->height - 1;
-                    }
+
                     double weight = exp(-(k * k + l * l) / (2 * sigma * sigma));
                     sum += src->data[y][x] * weight;
                     weight_sum += weight;
