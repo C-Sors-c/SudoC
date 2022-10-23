1925fb007c2e42db5123e09a2a454663a500c96e
Mickael Bobovitch <mickael.bobovitch@epita.fr>
2022-10-23 17:11:26 +0200

    main cleanup

--- sudoc/main.c
+++ sudoc/main.c
@@ -1,12 +1,6 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include "./include/utils.h"
-
-#define ERROR_INFO printf(RED "ERROR at: " YELLOW " %s:%d:%s()\n" RESET, __FILE__, __LINE__, __func__)
 
 int main(int argc, char *argv[])
 {
-    printf("Hello World!\n");
-    ERROR_INFO;
     return EXIT_SUCCESS;
 }
